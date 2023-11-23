37dc056836e93e0fa9a0b37f1b7847afc07325a8
Adam Ward <award@esri.com>
2023-11-22 16:08:45 -0800

    Fix log parameters

--- src/main/h265gstreamer.c
+++ src/main/h265gstreamer.c
@@ -340,6 +340,10 @@ void gstAndroidLog(GstDebugCategory * category,
                    GstDebugMessage  * message,
                    gpointer           data)
 {
+    (void)line;
+    (void)object;
+    (void)data;
+
     if (level <= gst_debug_category_get_threshold (category))
     {
         __android_log_print(ANDROID_LOG_ERROR, "SambasaDebug", "%s,%s: %s",
@@ -553,7 +557,7 @@ JNI_OnLoad (JavaVM * vm, void *reserved)
   //https://stackoverflow.com/questions/23550369/what-is-the-export-gst-debug-equivalent-for-android
   //https://gstreamer.freedesktop.org/documentation/gstreamer/gstinfo.html?gi-language=c#GstDebugLevel
   gst_debug_set_default_threshold( GST_LEVEL_DEBUG );
-  gst_debug_add_log_function(&gstAndroidLog, NULL);
+  gst_debug_add_log_function(&gstAndroidLog, NULL, NULL);
 
 
   char *version_utf8 = gst_version_string();
